7704f3f844bb3d3ea653cf1023aa8d50f2a0c5f8
RunningTHPed <pednoiiz008@gmail.com>
2023-06-10 02:58:02 +0700

    add footer ui

---
+++ src/component/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer
+      className="bg-black text-center lg:text-left">
+      <div className="p-4 text-center text-white">
+        © 2023 Sustainprenuer. All Rights Reserved.
+      </div>
+    </footer>
+  )
+}
+
